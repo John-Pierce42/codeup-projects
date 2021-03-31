0d98f281ecc751d177352b107b657e318e0cd307
John Pierce <pierce.johndaniel@gmail.com>
2021-03-30 22:02:08 -0500

    feat: practice search bar filter

---
+++ js/search-bar-filter.js
@@ -0,0 +1,43 @@
+(function (){
+ "use strict"
+
+
+
+var heroe = document.getElementsByClassName('hero-names')
+    heroe.addEventListener("keyup",function (e){
+
+    })
+
+ function loopHeroNames (){
+     for(var i = 0; i < heroes.length; i++){
+         heroes[i]
+     }
+ }
+
+
+
+
+
+
+
+    // var searchBar = document.querySelector('#search');
+    // function coffeeLoop (e){
+    //     e.preventDefault(); // don't submit the form, we just want to update the data
+    //     var coffeeSearch = searchBar.value.toLowerCase();
+    //     var filteredCoffeeNames = [];
+    //     for(var i = 0; i < coffees.length; i++){
+    //         var coffeeName = coffees[i].name;
+    //         if(coffeeName.toLowerCase().indexOf(coffeeSearch) >= 0) {
+    //             filteredCoffeeNames.push(coffees[i]);
+    //         }
+
+
+
+
+
+
+
+
+
+
+})();
