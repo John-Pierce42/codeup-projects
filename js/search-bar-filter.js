8266edeaf19a212729f2031ceea04ef0fe66eb7b
John Pierce <pierce.johndaniel@gmail.com>
2021-04-06 22:56:05 -0500

    feat: added img folder and still practicing search filters

--- js/search-bar-filter.js
+++ js/search-bar-filter.js
@@ -1,24 +1,39 @@
 (function (){
  "use strict"
+var heroArray =["Batman", "Superman", "Flash", "Cyborg", "Wonder Woman", "Aqua Man", "Hulk", "Rocket", "Spider Man", "Iron Man", "Ant Man", "Captain America", "Black Widow", "Thor", "Hawk Eye", "Groot", "Black Panther", "Scarlet Witch", "Wolverine", "Cyclops", "Professor X", "Beast", "Storm", "Jubilee", "Rogue","Gambit","Cable"];
 
 
+  var input = document.querySelector("#heroInput");
+  var heroes = document.getElementsByTagName("li");
+    console.log(heroes);
+    input.addEventListener("keyup",function (e){
 
-var heroe = document.getElementsByClassName('hero-names')
-    heroe.addEventListener("keyup",function (e){
+        var heroSearch = input.value.toLowerCase();
+        var filterHeroNames = [];
+        for(var i = 0; i < heroes.length; i++){
+            if(heroes[i].indexOf(heroSearch) >= 0){
+                filterHeroNames.push(heroes[i]);
+            }
+        }
 
     })
 
- function loopHeroNames (){
-     for(var i = 0; i < heroes.length; i++){
-         heroes[i]
-     }
- }
 
 
+// practicing filter--------
 
+// var heroe = document.getElementsByClassName('hero-names')
+//     heroe.addEventListener("keyup",function (e){
+//
+//     })
+//
+//  function loopHeroNames (){
+//      for(var i = 0; i < heroes.length; i++){
+//          heroes[i]
+//      }
+//  }
 
-
-
+ // code from coffee project-----------
 
     // var searchBar = document.querySelector('#search');
     // function coffeeLoop (e){
