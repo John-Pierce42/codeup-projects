8ac27e7eaf95b0d703176eccd31c266a7d696dbc
John Pierce <pierce.johndaniel@gmail.com>
2021-04-10 21:38:01 -0500

    feat: studying through search bar filter

--- js/search-bar-filter.js
+++ js/search-bar-filter.js
@@ -3,54 +3,25 @@
 var heroArray =["Batman", "Superman", "Flash", "Cyborg", "Wonder Woman", "Aqua Man", "Hulk", "Rocket", "Spider Man", "Iron Man", "Ant Man", "Captain America", "Black Widow", "Thor", "Hawk Eye", "Groot", "Black Panther", "Scarlet Witch", "Wolverine", "Cyclops", "Professor X", "Beast", "Storm", "Jubilee", "Rogue","Gambit","Cable"];
 
 
-  var input = document.querySelector("#heroInput");
-  var heroes = document.getElementsByTagName("li");
-    console.log(heroes);
-    input.addEventListener("keyup",function (e){
+ var searchBarInput = document.getElementById("heroInput");
+ var heroList = document.getElementById("heroes");
 
-        var heroSearch = input.value.toLowerCase();
-        var filterHeroNames = [];
-        for(var i = 0; i < heroes.length; i++){
-            if(heroes[i].indexOf(heroSearch) >= 0){
-                filterHeroNames.push(heroes[i]);
-            }
+function filterHeroes(){
+
+    var searchHero = searchBarInput.value.toLowerCase();
+    var filterHeroNames = "";
+    for(var i = 0; i < heroList.length; i++){
+        if(heroList[i].toLowerCase().indexOf(searchHero) >= 0){
+            filterHeroNames.push(heroList[i]);
+        } else {
+            filterHeroNames = "";
         }
+    }
 
-    })
-
-
-
-// practicing filter--------
-
-// var heroe = document.getElementsByClassName('hero-names')
-//     heroe.addEventListener("keyup",function (e){
-//
-//     })
-//
-//  function loopHeroNames (){
-//      for(var i = 0; i < heroes.length; i++){
-//          heroes[i]
-//      }
-//  }
-
- // code from coffee project-----------
-
-    // var searchBar = document.querySelector('#search');
-    // function coffeeLoop (e){
-    //     e.preventDefault(); // don't submit the form, we just want to update the data
-    //     var coffeeSearch = searchBar.value.toLowerCase();
-    //     var filteredCoffeeNames = [];
-    //     for(var i = 0; i < coffees.length; i++){
-    //         var coffeeName = coffees[i].name;
-    //         if(coffeeName.toLowerCase().indexOf(coffeeSearch) >= 0) {
-    //             filteredCoffeeNames.push(coffees[i]);
-    //         }
+}
 
 
-
-
-
-
+    searchBarInput.addEventListener("keyup",filterHeroes);
 
 
 
