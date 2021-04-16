904c9ce0b0812726b51684e2ce7e5d3666f6deaf
John Pierce <pierce.johndaniel@gmail.com>
2021-04-16 09:12:01 -0500

    feat: working on the filtering of heroes

--- js/search-bar-filter.js
+++ js/search-bar-filter.js
@@ -1,29 +1,77 @@
-(function (){
- "use strict"
-var heroArray =["Batman", "Superman", "Flash", "Cyborg", "Wonder Woman", "Aqua Man", "Hulk", "Rocket", "Spider Man", "Iron Man", "Ant Man", "Captain America", "Black Widow", "Thor", "Hawk Eye", "Groot", "Black Panther", "Scarlet Witch", "Wolverine", "Cyclops", "Professor X", "Beast", "Storm", "Jubilee", "Rogue","Gambit","Cable"];
+(function () {
+    "use strict"
+    var heroArray = [
+        {name: "Batman"},
+        {name: "Superman"},
+        {name: "Flash"},
+        {name: "Cyborg"},
+        {name: "Wonder Woman"},
+        {name: "Aqua Man"},
+        {name: "Hulk"},
+        {name: "Rocket"},
+        {name: "Spider Man"},
+        {name: "Iron Man"},
+        {name: "Ant Man"},
+        {name: "Captain America"},
+        {name: "Black Widow"},
+        {name: "Thor"},
+        {name: "Hawk Eye"},
+        {name: "Groot"},
+        {name: "Black Panther"},
+        {name: "Scarlet Witch"},
+        {name: "Wolverine"},
+        {name: "Cyclops"},
+        {name: "Professor X"},
+        {name: "Beast"},
+        {name: "Storm"},
+        {name: "Jubilee"},
+        {name: "Rogue"},
+        {name: "Gambit"},
+        {name: "Cable"}
+    ];
 
 
- var searchBarInput = document.getElementById("heroInput");
- var heroList = document.getElementById("heroes");
+    function renderHero(hero) {
+        var html = "<div class='container'>"
+        html += "<div class='row justify-content-center mt-5'>"
+        html += "<ul>";
+        html += "<li>" + hero.name + "</li>";
+        html += "</ul>"
+        html += "</div>"
+        html += "</div>"
 
-function filterHeroes(){
-
-    var searchHero = searchBarInput.value.toLowerCase();
-    var filterHeroNames = "";
-    for(var i = 0; i < heroList.length; i++){
-        if(heroList[i].toLowerCase().indexOf(searchHero) >= 0){
-            filterHeroNames.push(heroList[i]);
-        } else {
-            filterHeroNames = "";
-        }
+        return html;
     }
 
-}
+    function renderHeroes(heroes) {
+        var html = "";
+        for (var i = 0; i < heroes.length; i++) {
+            html += renderHero(heroes[i]);
+        }
+        return html;
+    }
 
 
-    searchBarInput.addEventListener("keyup",filterHeroes);
+    var searchBar = document.getElementById("search");
+    var submitBtn = document.getElementById("submit");
+
+    function filterHeroes() {
+
+        var searchHero = searchBarInput.value.toLowerCase();
+        var filterHeroNames = "";
+        console.log(heroList);
+        for (var i = 0; i < heroList.length; i++) {
+            if (heroList[i].toLowerCase().indexOf(searchHero) >= 0) {
+                filterHeroNames.push(heroList[i]);
+            } else {
+                filterHeroNames = "";
+            }
+        }
+
+    }
 
 
+    searchBarInput.addEventListener("keyup", filterHeroes);
 
 
 })();
